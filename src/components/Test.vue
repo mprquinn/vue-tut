3d62b08ddc79a214f5763c0e8235dab379a28084
Mike <mprquinn@gmail.com>
2017-04-18 13:47:18 -0600

    initial commit

---
+++ src/components/Test.vue
@@ -0,0 +1,27 @@
+<template>
+	<div class="test">
+		<h1>{{title}}</h1>
+		<p>
+			Welcome, {{user.firstName}}
+		</p>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'test',
+		data() {
+			return {
+				title: 'Hello World',
+				user: {
+					firstName: 'Jane',
+					lastName: 'Doe'
+				},
+				active: true
+			}
+		}
+	}
+</script>
+
+<style scoped>
+</style>
